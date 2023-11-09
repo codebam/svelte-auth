3434f329442df1b2d1639c08c48c7a8e4f1c5f5f
Sean Behan <codebam@riseup.net>
2023-11-09 03:33:01 -0500

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -10,10 +10,17 @@ export const actions = {
 		const data = await event.request.formData();
 		const email = data.get('email');
 		const password = await sha256(data.get('password')?.toString() ?? '');
+		const user = await event.platform?.env.DB.prepare('SELECT * FROM Users WHERE id=?')
+			.bind(email)
+			.all();
+		if (user.results[0]) {
+			return false;
+		}
 		await event.platform?.env.DB.prepare('INSERT INTO Users VALUES (?, ?)')
 			.bind(email, password)
 			.all();
 		event.cookies.set('session', JSON.stringify({ email, password }), { path: '/' });
+		return true;
 	},
 	login: async (event) => {
 		const data = await event.request.formData();
