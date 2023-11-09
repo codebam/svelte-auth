f5569447258ed50429aff36b0780909868e3401b
Sean Behan <codebam@riseup.net>
2023-11-09 14:57:01 -0500

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,39 +1,7 @@
-import { fail, redirect } from '@sveltejs/kit';
-
-const sha256 = async (text: string): Promise<string> =>
-	crypto.subtle.digest('SHA-256', new TextEncoder().encode(text)).then((array_buffer) =>
-		Array.from(new Uint8Array(array_buffer))
-			.map((b) => b.toString(16).padStart(2, '0'))
-			.join('')
-	);
+import { redirect } from '@sveltejs/kit';
 
 export const actions = {
-	register: async (event) => {
-		const data = await event.request.formData();
-		const email = data.get('email');
-		const password = await sha256(data.get('password')?.toString() ?? '');
-		const user = await event.platform?.env.DB.prepare('SELECT * FROM Users WHERE id=?')
-			.bind(email)
-			.all();
-		if (user.results[0]) {
-			return {};
-		}
-		await event.platform?.env.DB.prepare('INSERT INTO Users VALUES (?, ?)')
-			.bind(email, password)
-			.all();
-		event.cookies.set('session', JSON.stringify({ email, password }), { path: '/' });
-		throw redirect(303, '/');
-	},
-	login: async (event) => {
-		const data = await event.request.formData();
-		const email = data.get('email');
-		const password = await sha256(data.get('password')?.toString() ?? '');
-		const user = await event.platform?.env.DB.prepare('SELECT * FROM Users WHERE id=?')
-			.bind(email)
-			.all();
-		const db_password = user.results[0].password;
-		if (password === db_password) {
-			throw redirect(303, '/');
-		}
+	redirect: async (event) => {
+		throw redirect(303, '/success');
 	}
 };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,4 @@
-<h1>Login</h1>
-<form method="POST" action="?/login">
-  <label>Email
-    <input name="email" type="email">
-  </label>
-  <label>Password
-    <input name="password" type="password">
-  </label>
-  <button>Submit</button>
-</form>
-
-<h1>Register</h1>
-<form method="POST" action="?/register">
-  <label>Email
-    <input name="email" type="email">
-  </label>
-  <label>Password
-    <input name="password" type="password">
-  </label>
+<h1>Redirect</h1>
+<form method="POST" action="?/redirect">
   <button>Submit</button>
 </form>
