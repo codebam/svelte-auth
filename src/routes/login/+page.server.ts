7699f1c8444f133c72e64400d70f103891d9d90d
Sean Behan <codebam@riseup.net>
2023-11-09 03:28:56 -0500

    update

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,31 @@
+const sha256 = async (text: string): Promise<string> =>
+	crypto.subtle.digest('SHA-256', new TextEncoder().encode(text)).then((array_buffer) =>
+		Array.from(new Uint8Array(array_buffer))
+			.map((b) => b.toString(16).padStart(2, '0'))
+			.join('')
+	);
+
+export const actions = {
+	register: async (event) => {
+		const data = await event.request.formData();
+		const email = data.get('email');
+		const password = await sha256(data.get('password')?.toString() ?? '');
+		await event.platform?.env.DB.prepare('INSERT INTO Users VALUES (?, ?)')
+			.bind(email, password)
+			.all();
+		event.cookies.set('session', JSON.stringify({ email, password }), { path: '/' });
+	},
+	login: async (event) => {
+		const data = await event.request.formData();
+		const email = data.get('email');
+		const password = await sha256(data.get('password')?.toString() ?? '');
+		const user = await event.platform?.env.DB.prepare('SELECT * FROM Users WHERE id=?')
+			.bind(email)
+			.all();
+		const db_password = user.results[0].password;
+		if (password === db_password) {
+			return true;
+		}
+		return false;
+	}
+};
