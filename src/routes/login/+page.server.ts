f96218dca1205410e1f74b76d74b0dc59d1286c5
Sean Behan <codebam@riseup.net>
2023-11-09 15:37:24 -0500

    update

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,7 +1,22 @@
 import { redirect } from '@sveltejs/kit';
 
+const sha256 = async (text: string): Promise<string> =>
+	crypto.subtle.digest('SHA-256', new TextEncoder().encode(text)).then((array_buffer) =>
+		Array.from(new Uint8Array(array_buffer))
+			.map((b) => b.toString(16).padStart(2, '0'))
+			.join('')
+	);
+
 export const actions = {
-	redirect: async (event) => {
-		throw redirect(303, '/success');
+	register: async (event) => {
+		const data = await event.request.formData();
+		const email = data.get('email');
+		const password = await sha256(data.get('password')?.toString() ?? '');
+		const { success } = await event.platform?.env.DB.prepare('INSERT INTO Users VALUES (?, ?)')
+			.bind(email, password)
+			.all();
+		if (success) {
+			throw redirect(303, '/');
+		}
 	}
 };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Redirect</h1>
-<form method="POST" action="?/redirect">
+<h1>Register</h1>
+<form method="POST" action="?/register">
+  <label>Email
+    <input name="email" type="email">
+  </label>
+  <label>Password
+    <input name="password" type="password">
+  </label>
   <button>Submit</button>
 </form>
