ed67d67d4921883b428d48ba5d70113b5a527d6d
Sean Behan <codebam@riseup.net>
2023-11-10 16:48:42 -0500

    Revert "update"
    This reverts commit 868e13398faab71699b0f21327a7aec31b4508cd.

--- functions/worker.ts
+++
@@ -1,9 +0,0 @@
-export default {
-	scheduled: async (request, env, ctx) => {
-		const result = await env.PLAYLIST_DB.prepare('SELECT id FROM Playlist').all();
-		const { success } = await env.PLAYLIST_DB.prepare('DELETE FROM Playlist WHERE id=?')
-			.bind(result.results[result.results.length - 1].id)
-			.all();
-		return new Response('ok');
-	}
-};
