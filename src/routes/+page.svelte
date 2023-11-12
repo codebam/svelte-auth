b2c101d085537dbfa202716654b9e8362852a20d
Sean Behan <codebam@riseup.net>
2023-11-12 00:36:20 -0500

    "enhance" the form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
   import YouTube from '$lib/svelte-youtube.svelte';
   export let data: {auth: boolean, song: {id: string, url: string}};
-  const removeSong = () => {
-    document.getElementById('remove_song').click();
-  };
 </script>
 
 {#if !data.auth}
 <p>Visit <a href="/login">login</a></p>
 {/if}
 
-<YouTube on:end={() => removeSong()} videoId={data.song.url} options={{playerVars: { autoplay: 1 }}} />
+<YouTube on:end={() => document.getElementById('remove_song')?.click()} videoId={data.song.url} options={{playerVars: { autoplay: 1 }}} />
 
-<form style="display: none;" method="POST" action="/playlist?/remove">
+<form style="display: none;" method="POST" action="/playlist?/remove" use:enhance>
   <input name="id" value="{data.song.id}">
   <button id="remove_song">remove</button>
 </form>
 
 {#if data.auth}
-  <form method="POST" action="/playlist?/add">
+  <form method="POST" action="/playlist?/add" use:enhance>
     <label>Submit a song
       <input name="url" type="url" placeholder="paste a youtube link" pattern=".*v=.*">
     </label>
