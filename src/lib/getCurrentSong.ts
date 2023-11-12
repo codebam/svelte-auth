736a14b4e9bd88b42bf11178362ce2dec3a7f996
Sean Behan <codebam@riseup.net>
2023-11-11 20:51:07 -0500

    update

--- src/lib/getCurrentSong.ts
+++ src/lib/getCurrentSong.ts
@@ -1,8 +1,10 @@
-const getCurrentSong = async (event) => {
-	const result = await event.platform.env.DB.prepare(
-		'SELECT url FROM Playlist ORDER BY date DESC LIMIT 1'
-	).all();
-	return result.results[0].url;
+import type { D1Database } from '@cloudflare/workers-types';
+
+const getCurrentSong = async (db: D1Database | undefined) => {
+	if (db) {
+		const result = await db.prepare('SELECT url FROM Playlist ORDER BY date DESC LIMIT 1').all();
+		return result.results[0].url;
+	}
 };
 
 export default getCurrentSong;
